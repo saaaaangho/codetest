341305270b2fd1fe56774085def52836bb0bb24b
saaaaangho <dark13658341@gmail.com>
2025-04-04 10:48:13 +0900

    [level 4] Title: 언어별 개발자 분류하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
+++ 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
@@ -1,13 +1,31 @@
+with sum_fe as (
+  select sum(CODE) as FE from SKILLCODES
+  where CATEGORY = 'Front End'
+),
 
-with grade as (
+python as (
+  select CODE as python from SKILLCODES
+  where NAME = 'Python'
+),
+
+csharp as (
+  select CODE as csharp from SKILLCODES
+  where NAME = 'C#'
+),
+
+grade as (
   select ID, EMAIL,
          case
-           when (SKILL_CODE & (select sum(CODE) from SKILLCODES where CATEGORY = 'Front end')) <> 0 and (SKILL_CODE & 256) <> 0 then 'A'
-           when (SKILL_CODE & 1024) <> 0 then 'B'
-           when (SKILL_CODE & (select sum(CODE) from SKILLCODES where CATEGORY = 'Front end')) <> 0 then 'C'
+           when (SKILL_CODE & fe.FE <> 0) 
+                and (SKILL_CODE & py.python <> 0) then 'A'
+           when (SKILL_CODE & cs.csharp <> 0) then 'B'
+           when (SKILL_CODE & fe.FE <> 0) then 'C'
            else null
          end as GRADE
-         from DEVELOPERS
+  from DEVELOPERS
+  cross join sum_fe fe
+  cross join python py
+  cross join csharp cs
 )
 
 select GRADE, ID, EMAIL from grade
