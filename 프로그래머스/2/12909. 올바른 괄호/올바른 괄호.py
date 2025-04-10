f63d498ea26f17875e89f41587ca8d7bfacb4919
saaaaangho <dark13658341@gmail.com>
2025-04-10 10:26:33 +0900

    [level 2] Title: 올바른 괄호, Time: 6.35 ms, Memory: 9.44 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,13 +1,14 @@
 def solution(s):
     stack = []
+    
     for i in s:
         if i == '(':
             stack.append(i)
         else:
             if not stack:
                 return False
-            stack.pop()
-            
+            else:
+                stack.pop()
     return len(stack) == 0
         
 
