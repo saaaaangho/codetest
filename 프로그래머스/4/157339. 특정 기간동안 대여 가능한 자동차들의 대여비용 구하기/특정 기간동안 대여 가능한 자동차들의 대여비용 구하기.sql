41f8c81d521b8409d8b44aef77f8e1758d2cf0ae
saaaaangho <dark13658341@gmail.com>
2025-04-13 22:03:17 +0900

    [level 4] Title: 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
+++ 프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
@@ -1,30 +1,20 @@
-with car_type_filterd as (
-  select CAR_ID, CAR_TYPE, DAILY_FEE, OPTIONS
-  from CAR_RENTAL_COMPANY_CAR
+with car_filterd as (
+  select * from CAR_RENTAL_COMPANY_CAR
   where CAR_TYPE in ('세단', 'SUV')
 ),
 
-rental_filterd as (
-  select rh.HISTORY_ID, rh.CAR_ID, rh.START_DATE, rh.END_DATE
-  from CAR_RENTAL_COMPANY_RENTAL_HISTORY rh
-  join car_type_filterd ctf on ctf.CAR_ID = rh.CAR_ID
-  where rh.END_DATE >= '2022-11-01' and rh.START_DATE <= '2022-11-30'
-),
-
-discount_plan_filterd as (
-  select PLAN_ID, CAR_TYPE, DURATION_TYPE, DISCOUNT_RATE
-  from CAR_RENTAL_COMPANY_DISCOUNT_PLAN
-  where DURATION_TYPE = '30일 이상'
+day_filterd as (
+  select distinct rh.CAR_ID from CAR_RENTAL_COMPANY_RENTAL_HISTORY rh
+  join car_filterd cf on cf.CAR_ID = rh.CAR_ID
+  where not (rh.START_DATE > '2022-11-30' or rh.END_DATE < '2022-11-01')
 )
 
-select ctf.CAR_ID, ctf.CAR_TYPE, FLOOR(ctf.DAILY_FEE * (1 - dpf.DISCOUNT_RATE / 100) * 30) AS FEE
-from car_type_filterd ctf
-join discount_plan_filterd dpf on dpf.CAR_TYPE = ctf.CAR_TYPE
-where ctf.CAR_ID NOT IN (SELECT CAR_ID FROM rental_filterd)
-  and FLOOR(ctf.DAILY_FEE * (1 - dpf.DISCOUNT_RATE / 100) * 30) between 500000 and 1999999
-order by (ctf.DAILY_FEE*dpf.DISCOUNT_RATE)*30 desc, ctf.CAR_TYPE asc, ctf.CAR_ID desc;
-       # case when cc.CAR_TYPE = '세단' then (cc.DAILY_FEE*dpf.DISCOUNT_RATE)*30
-       #      when cc.CAR_TYPE = 'SUV' then (cc.DAILY_FEE*dpf.DISCOUNT_RATE)*30
+select cf.CAR_ID, cf.CAR_TYPE,
+       floor((1-dp.DISCOUNT_RATE/100) * (cf.DAILY_FEE*30)) as FEE
+       from car_filterd cf
+join CAR_RENTAL_COMPANY_DISCOUNT_PLAN dp on cf.CAR_TYPE = dp.CAR_TYPE and dp.DURATION_TYPE = '30일 이상'
+where cf.CAR_ID not in (select CAR_ID from day_filterd)
+  and floor((1-dp.DISCOUNT_RATE/100) * (cf.DAILY_FEE*30)) between 500000 and 2000000
 
 
-  
+
